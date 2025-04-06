a5d53c85da306aba037e7d17914c60d1eb547a32
Gustavo Sequeira <sequeira2003@gmail.com>
2025-04-05 21:43:22 -0300

    pagina de pedidos

---
+++ alurabooks/src/componentes/AreaLogada/index.tsx
@@ -0,0 +1,16 @@
+import { Link, Outlet } from "react-router-dom"
+import './AreaLogada.css'
+
+const AreaLogada = () => {
+    return (
+        <>
+            Minha conta
+            Pedidos
+            Trocas
+            Cupons
+            Seus dados
+        </>
+    )
+}
+
+export default AreaLogada
--- alurabooks/src/rotas/index.tsx
+++ alurabooks/src/rotas/index.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "../paginas/Home"
 import PaginaBase from "../paginas/PaginaBase"
+//import AreaLogada from "../paginas/AreaLogada" 
+//import Pedidos from "../paginas/Pedidos" 
 
 
 const Rotas = () => {
