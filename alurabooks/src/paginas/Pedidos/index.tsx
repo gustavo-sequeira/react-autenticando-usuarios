f21cda6d9fc51fc1fb1eb99143bd50e125b1d4ae
Gustavo Sequeira <sequeira2003@gmail.com>
2025-04-07 19:41:47 -0300

    implementando o css de pedidos

--- alurabooks/src/paginas/Pedidos/index.tsx
+++ alurabooks/src/paginas/Pedidos/index.tsx
@@ -1,15 +1,91 @@
+import { AbBotao } from "ds-alurabooks"
+import './Pedidos.css'  
+
 const Pedidos = () => {
     return (
         <section className="pedidos">
             <h1>Meus pedidos</h1>
-            <div>
+            <div className="pedido">
                 <ul>
                     <li>Pedido: <strong>12345</strong></li>
                     <li>Data do pedido: <strong>12/09/2025</strong></li>
                     <li>Valor total: <strong>R$ 42.00</strong></li>
                     <li>Entrega realizada em:: <strong>15/09/2025</strong></li>
                 </ul>
+
+                <AbBotao texto="Detalhes" />
             </div >
+            <div className="pedido">
+                <ul>
+                    <li>Pedido: <strong>12345</strong></li>
+                    <li>Data do pedido: <strong>12/09/2025</strong></li>
+                    <li>Valor total: <strong>R$ 42.00</strong></li>
+                    <li>Entrega realizada em:: <strong>15/09/2025</strong></li>
+                </ul>
+
+                <AbBotao texto="Detalhes" />
+            </div >
+            <div className="pedido">
+                <ul>
+                    <li>Pedido: <strong>12345</strong></li>
+                    <li>Data do pedido: <strong>12/09/2025</strong></li>
+                    <li>Valor total: <strong>R$ 42.00</strong></li>
+                    <li>Entrega realizada em:: <strong>15/09/2025</strong></li>
+                </ul>
+
+                <AbBotao texto="Detalhes" />
+            </div >
+            <div className="pedido">
+                <ul>
+                    <li>Pedido: <strong>12345</strong></li>
+                    <li>Data do pedido: <strong>12/09/2025</strong></li>
+                    <li>Valor total: <strong>R$ 42.00</strong></li>
+                    <li>Entrega realizada em:: <strong>15/09/2025</strong></li>
+                </ul>
+
+                <AbBotao texto="Detalhes" />
+            </div >
+            <div className="pedido">
+                <ul>
+                    <li>Pedido: <strong>12345</strong></li>
+                    <li>Data do pedido: <strong>12/09/2025</strong></li>
+                    <li>Valor total: <strong>R$ 42.00</strong></li>
+                    <li>Entrega realizada em:: <strong>15/09/2025</strong></li>
+                </ul>
+
+                <AbBotao texto="Detalhes" />
+            </div >
+            <div className="pedido">
+                <ul>
+                    <li>Pedido: <strong>12345</strong></li>
+                    <li>Data do pedido: <strong>12/09/2025</strong></li>
+                    <li>Valor total: <strong>R$ 42.00</strong></li>
+                    <li>Entrega realizada em:: <strong>15/09/2025</strong></li>
+                </ul>
+
+                <AbBotao texto="Detalhes" />
+            </div >
+            <div className="pedido">
+                <ul>
+                    <li>Pedido: <strong>12345</strong></li>
+                    <li>Data do pedido: <strong>12/09/2025</strong></li>
+                    <li>Valor total: <strong>R$ 42.00</strong></li>
+                    <li>Entrega realizada em:: <strong>15/09/2025</strong></li>
+                </ul>
+
+                <AbBotao texto="Detalhes" />
+            </div >
+            <div className="pedido">
+                <ul>
+                    <li>Pedido: <strong>12345</strong></li>
+                    <li>Data do pedido: <strong>12/09/2025</strong></li>
+                    <li>Valor total: <strong>R$ 42.00</strong></li>
+                    <li>Entrega realizada em:: <strong>15/09/2025</strong></li>
+                </ul>
+
+                <AbBotao texto="Detalhes" />
+            </div >
+            
     </section >) 
 }
 
