713dc0fffdadfd909bffff1e793eceff33a13fb3
Gustavo Sequeira <sequeira2003@gmail.com>
2025-04-08 16:28:11 -0300

    implementando o logout

--- alurabooks/src/componentes/BarraNavegacao/index.tsx
+++ alurabooks/src/componentes/BarraNavegacao/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import BotaoNavegacao from "../BotaoNavegacao"
 import ModalCadastroUsuario from "../ModalCadastroUsuario"
 import logo from './assets/logo.png'
@@ -12,12 +12,20 @@ const BarraNavegacao = () => {
     const [modalCadastroAberta, setModalCadastroAberta] = useState(false)
     const [modalLoginAberta, setModalLoginAberta] = useState(false)
 
+    let navigate = useNavigate();
+
     const token = sessionStorage.getItem('token')
     const [usuarioEstaLogado, setUsuarioEstaLogado] = useState<boolean>(token != null)
 
     const aoEfetuarLogin = () => {
         setModalLoginAberta(false)
         setUsuarioEstaLogado(true)
+    }
+
+    const aoEfettuarLogout = () => {
+        setUsuarioEstaLogado(false)
+        sessionStorage.removeItem('token')
+        navigate('/')
     }
 
     return (<nav className="ab-navbar">
@@ -85,6 +93,14 @@ const BarraNavegacao = () => {
                 <>
                     <li>
                         <Link to='/area-logada'>Minha Conta</Link>
+                    </li>
+                    <li>
+                    <BotaoNavegacao
+                        texto="Logout"
+                        textoAltSrc="Icone representando um usuário"
+                        imagemSrc={usuario}
+                        onClick={aoEfettuarLogout}
+                    />
                     </li>
                 </>
             }
