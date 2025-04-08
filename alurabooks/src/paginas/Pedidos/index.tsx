5e52de1c15b5e9ecb2a3b6a63828712ea1bac59d
Gustavo Sequeira <sequeira2003@gmail.com>
2025-04-08 15:22:02 -0300

    exclusão de pedidos

--- alurabooks/src/paginas/Pedidos/index.tsx
+++ alurabooks/src/paginas/Pedidos/index.tsx
@@ -22,6 +22,19 @@ const Pedidos = () => {
             .catch(erro => console.log(erro))
     }, [])
 
+    const excluir = (pedido: IPedido) => {
+        const token = sessionStorage.getItem('token')
+        axios.delete('http://localhost:8000/pedidos/' + pedido.id, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(() => {
+            setPedidos(pedidos.filter(p => p.id != pedido.id))
+                })
+        .catch(erro => console.log(erro))
+    }
+
     return (
         <section className="pedidos">
             <h1>Meus pedidos</h1>
@@ -37,6 +50,9 @@ const Pedidos = () => {
                                             </strong></li>
 
                             <li>Entrega realizada em:: <strong> {new Date(pedido.entrega).toLocaleDateString()} </strong></li>
+                            <li>
+                                <button onClick={() => excluir(pedido)}>Excluir</button>
+                            </li>
                         </ul>
 
                         <AbBotao texto="Detalhes" />
